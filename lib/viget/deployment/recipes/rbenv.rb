82dc507fe1dd5dcadfc340b63b2f33a9672adbfc
Patrick Reagan <reaganpr@gmail.com>
2014-02-12 14:43:59 -0700

    Update rbenv paths

--- lib/viget/deployment/recipes/rbenv.rb
+++ lib/viget/deployment/recipes/rbenv.rb
@@ -1,1 +1,3 @@
-set :default_environment, {'PATH' => '/opt/rbenv/shims:/opt/rbenv/bin:$PATH'}
+set :default_environment, {
+  'PATH' => '/opt/rbenv/shims:/usr/local/rbenv/shims:/opt/rbenv/bin:/usr/local/rbenv/bin:$PATH'
+}
